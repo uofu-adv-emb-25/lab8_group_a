319dc12fa64b28291f6116f4594ec726aefeb510
Luke Stillings <44128419+lukestillings@users.noreply.github.com>
2025-10-29 12:49:57 -0600

    Send and receive messages

--- src/can.c
+++ src/can.c
@@ -4,14 +4,20 @@
 
 #include <FreeRTOS.h>
 #include <task.h>
+#include <queue.h>
 #include <pico/stdlib.h>
 #include <can2040.h>
 
+#define QUEUE_LENGTH 32
+
+static QueueHandle_t queue;
 static struct can2040 cbus;
 
 static void can2040_cb(struct can2040 *cd, uint32_t notify, struct can2040_msg *msg)
 {
-    // Put your code here....
+    if (notify == CAN2040_NOTIFY_RX) {
+        xQueueSend(queue, msg, portMAX_DELAY);
+    }
 }
 
 static void PIOx_IRQHandler(void)
@@ -24,6 +30,9 @@ static void canbus_setup(void)
     uint32_t pio_num = 0;
     uint32_t sys_clock = 125000000, bitrate = 500000;
     uint32_t gpio_rx = 4, gpio_tx = 5;
+
+    // Setup queue
+    queue = xQueueCreate(QUEUE_LENGTH, sizeof (struct can2040_msg));
 
     // Setup canbus
     can2040_setup(&cbus, pio_num);
@@ -38,9 +47,25 @@ static void canbus_setup(void)
     can2040_start(&cbus, sys_clock, bitrate, gpio_rx, gpio_tx);
 }
 
-static void main_task(void *params)
+static void send_task(void *params)
 {
     while (1) {
+        struct can2040_msg msg;
+        msg.id = 0;
+        msg.dlc = 8;
+        msg.data32[0] = 0x01234567;
+        msg.data32[1] = 0x89abcdef;
+        can2040_transmit(&cbus, &msg);
+        vTaskDelay(1000);
+    }
+}
+
+static void recv_task(void *params)
+{
+    while (1) {
+        struct can2040_msg msg;
+        xQueueReceive(queue, &msg, portMAX_DELAY);
+        printf("received message: id=%d dlc=%d data32[0]=%d data32[1]=%d\n", msg.id, msg.dlc, msg.data32[0], msg.data32[1]);
     }
 }
 
@@ -49,6 +74,7 @@ int main(void)
     stdio_init_all();
     canbus_setup();
 
-    xTaskCreate(main_task, "main", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY+1, NULL);
+    xTaskCreate(send_task, "send", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY+1, NULL);
+    xTaskCreate(recv_task, "recv", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY+1, NULL);
     vTaskStartScheduler();
 }
